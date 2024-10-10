e43b791c07d9fafd3ef5f4d40895570246e47651
andrey dolgih <12dolg12@inbox.ru>
2024-10-10 22:19:31 +0300

    Add project files.

---
+++ Module3.CLR/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Module3.CLR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Andrew";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+
+    }
+}
