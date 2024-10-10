341c25e675a190beacae81ca69e10307c01a7b7d
andrey dolgih <12dolg12@inbox.ru>
2024-10-10 23:44:09 +0300

    task 3.2.8

--- Module3.CLR/Program.cs
+++ Module3.CLR/Program.cs
@@ -1,12 +1,20 @@
-﻿namespace Module3.CLR
+﻿using System;
+
+namespace Module3.CLR
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string MyName = "Andrew";
+            const string MyName = "Andrew";
 
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет, мир");
+            Console.WriteLine("\t Мне 25 лет");
+            Console.WriteLine("\t My name is \n Andrew");
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23');
 
             Console.ReadKey();
         }
