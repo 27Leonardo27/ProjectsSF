411ddacb093774497f5b7da6cee489860edf02d4
andrey dolgih <12dolg12@inbox.ru>
2024-10-27 23:00:43 +0300

    task 4.3.14

--- Module4.CLR/Program.cs
+++ Module4.CLR/Program.cs
@@ -6,16 +6,19 @@ namespace Module4.CLR
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4, };
+            int[][] array = new int[3][];
 
-            int amount = 0;
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-            for (int i = 0; i < arr.Length; i++)
+            foreach (var num in array)
             {
-                amount += arr[i];
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
             }
-
-            Console.WriteLine(amount);
         }
     }
 }
