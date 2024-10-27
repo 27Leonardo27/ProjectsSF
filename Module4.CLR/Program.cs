235e1058cbfc696cccd386a20cc4e1a884159746
andrey dolgih <12dolg12@inbox.ru>
2024-10-27 22:53:28 +0300

    task 4.3.13

--- Module4.CLR/Program.cs
+++ Module4.CLR/Program.cs
@@ -8,24 +8,14 @@ namespace Module4.CLR
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4, };
 
-            for (int a = 0; a < arr.Length; a++)
-            {
-                for (int b = a + 1; b < arr.Length; b++)
-                {
-                    if (arr[a] > arr[b])
-                    {
-                        int c = arr[a];
-                        arr[a] = arr[b];
-                        arr[b] = c;
-                    }
-                }
+            int amount = 0;
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                amount += arr[i];
             }
 
-            foreach (var order in arr)
-            {
-                Console.Write(order);
-            }
+            Console.WriteLine(amount);
         }
     }
 }
