094b2bcac92734ac66b8998483177243e1e65930
andrey dolgih <12dolg12@inbox.ru>
2024-11-11 00:51:07 +0300

    task 6.3.1 - 6.3.2

--- Module6.OOP/Program.cs
+++ Module6.OOP/Program.cs
@@ -1,109 +1,163 @@
-﻿namespace Module6.OOP
+﻿namespace Module6.OOP;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var department = GetCurrentDepartment();
+
+        if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
         {
-            Human human = new Human();
-            human.Greetings();
+            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+        }
 
-            human = new Human("Дмитрий");
-            human.Greetings();
+        /*Human human = new Human();
+        human.Greetings();
 
-            human = new Human("Дмитрий", 23);
-            human.Greetings();
+        human = new Human("Дмитрий");
+        human.Greetings();
 
-            Console.ReadKey();
+        human = new Human("Дмитрий", 23);
+        human.Greetings();
+
+        Console.ReadKey();*/
+    }
+  
+    static Department GetCurrentDepartment()
+    {       
+        var department = new Department();
+
+        department.City = new City();
+        department.City.Name = "Санкт-Петербург";
+
+        department.Company = new Company();
+        department.Company.Name = "Sber";
+        department.Company.Type = "Банк";
+
+        return department;
+    }
+}
+
+class Bus
+{
+    public int? Load;
+
+    public void PrintStatus()
+    {
+        if (Load.HasValue && Load > 0)
+        {
+            Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
+        }
+        else
+        {
+            Console.WriteLine("Автобус пуст!");
         }
     }
+}
 
-    class Human
+class Human
+{
+    public string name;
+    public int age;
+
+    public void Greetings()
     {
-        public string name;
-        public int age;
-
-        public void Greetings()
-        {
-            Console.WriteLine("My name is {0}, my age is {1}", name, age);
-        }
-
-        public Human()
-        {
-            name = "Unknow";
-            age = 20;
-        }
-
-        public Human(string n)
-        {
-            name = n;
-            age = 20;
-        }
-
-        public Human(string n, int a)
-        {
-            name = n;
-            age = a;
-        }
+        Console.WriteLine("My name is {0}, my age is {1}", name, age);
     }
 
-    struct Animal
+    public Human()
     {
-        public string type;
-        public string name;
-        public int age;
-
-        public void Info()
-        {
-            Console.WriteLine("This is {0} named {1} he is {2} old");
-        }
+        name = "Unknow";
+        age = 20;
     }
 
-    class Pen
+    public Human(string n)
     {
-        public string color;
-        public int cost;
-
-        public Pen()
-        {
-            color = "Черный";
-            cost = 100;
-        }
-
-        public Pen(string penColor, int penCost)
-        {
-            color = penColor;
-            cost = penCost;
-        }
-
+        name = n;
+        age = 20;
     }
 
-    class Rectangle
+    public Human(string n, int a)
     {
-        public int a;
-        public int b;
-
-        public Rectangle()
-        {
-            a = 6;
-            b = 4;
-        }
-
-        public Rectangle(int side)
-        {
-            a = side;
-            b = side;
-        }
-
-        public Rectangle(int first, int second)
-        {
-            a = first;
-            b = second;
-        }
-
-        public int Square()
-        {
-            return a * b;   
-        }
-
+        name = n;
+        age = a;
     }
+}
+
+struct Animal
+{
+    public string type;
+    public string name;
+    public int age;
+
+    public void Info()
+    {
+        Console.WriteLine("This is {0} named {1} he is {2} old");
+    }
+}
+
+class Pen
+{
+    public string color;
+    public int cost;
+
+    public Pen()
+    {
+        color = "Черный";
+        cost = 100;
+    }
+
+    public Pen(string penColor, int penCost)
+    {
+        color = penColor;
+        cost = penCost;
+    }
+
+}
+
+class Rectangle
+{
+    public int a;
+    public int b;
+
+    public Rectangle()
+    {
+        a = 6;
+        b = 4;
+    }
+
+    public Rectangle(int side)
+    {
+        a = side;
+        b = side;
+    }
+
+    public Rectangle(int first, int second)
+    {
+        a = first;
+        b = second;
+    }
+
+    public int Square()
+    {
+        return a * b;   
+    }
+
+}
+
+class Company
+{
+    public string Type;
+    public string Name;
+}
+
+class Department
+{
+    public Company Company;
+    public City City;
+}
+
+class City
+{
+    public string Name;
 }
