735ec60d4010d3e5b7cf05c302ebc094aec08aa4
andrey dolgih <12dolg12@inbox.ru>
2024-12-13 00:01:39 +0300

    Home_work

---
+++ Module9.EventSort/InvalidSortTypeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module9.EventSort
+{
+    internal class InvalidSortTypeException : Exception
+    {
+        public InvalidSortTypeException(string message)
+            : base(message) 
+        {
+
+        }
+    }
+}
---
+++ Module9.EventSort/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Module9.EventSort
+{
+    internal class Program
+    {
+        public static event Action<string[]> OnSort;
+
+        private static string[] lastNames =
+        {
+            "Elfie",
+            "Gerson",
+            "Bond",
+            "Willis",
+            "Parker"
+        };
+
+        static void Main(string[] args)
+        {
+            OnSort += Sort;
+
+            Console.WriteLine("Before sort: ");
+            foreach(var lastName in lastNames)
+            {
+                Console.WriteLine(lastName);
+            }
+
+            try
+            {
+                OnSort?.Invoke(lastNames);
+            }
+            catch (InvalidSortTypeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\nAfter sort: ");
+            foreach(var lastName in lastNames)
+            {
+                Console.WriteLine(lastName);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Sort(string[] lastNames)
+        {
+            Console.Write("Enter sort type (1/2): ");
+            var sortType = Console.ReadLine();
+
+            if (sortType != "1" && sortType != "2")
+                throw new InvalidSortTypeException("Sort type must be '1' or '2'");
+
+            Array.Sort(lastNames);
+
+            if ( sortType == "2")
+                Array.Reverse(lastNames);
+        }
+    }
+}
