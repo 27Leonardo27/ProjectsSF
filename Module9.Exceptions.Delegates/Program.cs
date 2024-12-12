0f33801ac98e3124d0101116d9b500b9f07b5ca8
andrey dolgih <12dolg12@inbox.ru>
2024-12-12 23:59:16 +0300

    Home_work

---
+++ Module9.Exceptions.Delegates/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Module9.Exceptions.Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exceptions = new Exception[5];
+            exceptions[0] = new NegativeAgeException("Неверный возраст!");
+            exceptions[1] = new Exception("Исключение");
+            exceptions[2] = new DivideByZeroException("Делить на ноль нельзя!");
+            exceptions[3] = new NotImplementedException("Метод не реализован!");
+            exceptions[4] = new ArgumentNullException("Пустой аргумент!");
+
+            foreach (var exception in exceptions) 
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (NegativeAgeException ex)
+                {
+                    Console.WriteLine("NegativeAgeException");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("DivideByZeroException");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (NotImplementedException ex)
+                {
+                    Console.WriteLine("NotImplementedException");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("ArgumentNullException");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+        }
+
+    }
+
+    class NegativeAgeException : Exception 
+    {
+        public NegativeAgeException(string message)
+            : base(message)
+        { 
+            
+        }
+    }
+}
