9c76adf5177a3d3ded9bd271666bbdb139f2aef0
andrey dolgih <12dolg12@inbox.ru>
2024-12-19 22:33:44 +0300

    Home_work 10.5.1

---
+++ Module10.Interfaces/ISum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.Interfaces
+{
+    interface ISum
+    {
+        int Sum(int a,  int b);
+        
+    }
+
+    class Calculator : ISum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Module10.Interfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Module10.Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                var a = 0;
+                var b = 0;
+
+                try
+                {
+                    Console.Write("Первое число: ");
+                    a = int.Parse(Console.ReadLine());
+
+                    Console.Write("Второе число: ");
+                    b = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ошибка при вводе числа!");
+                    continue;
+                }
+
+                var calculator = new Calculator();
+                var result = calculator.Sum(a, b);
+
+                Console.WriteLine($"Результат: {result}");
+
+            } while (true);
+        }
+    }
+}
