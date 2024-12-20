de26ab9f97500e09fd644e10f10aac48ed24816c
andrey dolgih <12dolg12@inbox.ru>
2024-12-20 23:36:15 +0300

    Sort_Repository

--- Module6.OOP/Program.cs
+++ Module6.OOP/Program.cs
@@ -22,7 +22,7 @@ internal class Program
 
         Console.ReadKey();*/
     }
-  
+      
     static Department GetCurrentDepartment()
     {       
         var department = new Department();
--- Module7.OOP.Continue/Program.cs
+++ Module7.OOP.Continue/Program.cs
@@ -67,4 +67,45 @@
         private string ProgrammingLanguage;
     }
 
+    class Food 
+    {
+
+    }
+
+    class Fruit : Food 
+    {
+
+    }
+
+    class Vegetable : Food
+    { 
+
+    }
+
+    class Apple : Fruit
+    {
+
+    }
+
+    class Banana : Fruit
+    {
+
+    }
+
+    class Pear : Fruit 
+    { 
+
+    }
+
+    class Potato : Vegetable
+    {
+
+    }
+
+    class Carrot : Vegetable
+    { 
+
+    }
+
+    
 }
