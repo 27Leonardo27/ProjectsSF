01c491d608054e963a9e0d3266730e9cbd8a1d85
andrey dolgih <12dolg12@inbox.ru>
2025-01-03 19:48:51 +0300

    task 12.1.13

---
+++ Module12.Algorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Module12.Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much elements must be in array?");
+            var count = int.Parse(Console.ReadLine());
+
+            var array = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("All elements are written");
+        }
+    }
+}
