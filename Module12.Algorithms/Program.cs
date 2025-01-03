939cc007873be50c3f1cd49627749770916aa899
andrey dolgih <12dolg12@inbox.ru>
2025-01-04 00:12:42 +0300

    Home_work 12.1.5

--- Module12.Algorithms/Program.cs
+++ Module12.Algorithms/Program.cs
@@ -1,20 +1,80 @@
-﻿namespace Module12.Algorithms
+﻿using System.Runtime.CompilerServices;
+
+namespace Module12.Algorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How much elements must be in array?");
-            var count = int.Parse(Console.ReadLine());
-
-            var array = new string[count];
-
-            for (int i = 0; i < count; i++)
+            var user1 = new User()
             {
-                array[i] = Console.ReadLine();
+                Name = "Ivan",
+                Login = "Iv228",
+                IsPremium = false
+            };
+
+            var user2 = new User() 
+            {
+                Name = "Andrew", 
+                Login = "AndromedA", 
+                IsPremium = false 
+            };
+
+            var user3 = new User() 
+            {
+                Name = "James",
+                Login = "_GoAt_", 
+                IsPremium = true 
+            };
+
+            Console.WriteLine($"Hello, {user1.Name}");
+
+            if (!user1.IsPremium)
+            {
+                ShowAds();
+            }
+            else 
+            { 
+                Console.WriteLine("Your subscription is active!");
             }
 
-            Console.WriteLine("All elements are written");
+            Console.WriteLine($"Hello, {user2.Name}");
+
+            if (!user2.IsPremium)
+            {
+                ShowAds();
+            }
+            else 
+            {
+                Console.WriteLine("Your subscription is active!");
+            }
+
+            Console.WriteLine($"Hello, {user3.Name}");
+
+            if (!user3.IsPremium)
+            {
+                ShowAds();
+            }
+            else
+            {
+                Console.WriteLine("Your subscription is active!");
+            }
+
+            Console.ReadKey();            
         }
-    }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+
+    }   
 }
+         
---
+++ Module12.Algorithms/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12.Algorithms
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public  string Name { get; set; }
+        public bool IsPremium { get; set; }
+        
+    }
+
+}
