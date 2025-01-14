4ffd149102d63d8ec79f1f762c6a1f9002f32e41
andrey dolgih <12dolg12@inbox.ru>
2025-01-14 22:26:48 +0300

    task 13.1.4

---
+++ Module13.Collections/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Module13.Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, -5, 0, 23, -4 };
+            Console.WriteLine(CheckArray(array));
+        }
+
+        static bool CheckArray(int[] numbers)
+        {
+            //  используем цикл for для обхода массива
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                //  проверяем следующий элемент на предмет того, что он меньше предыдушего
+                if (numbers[i + 1] < numbers[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
