c60349dcdf3b5b469af91ca77648b5ef30d12cd6
andrey dolgih <12dolg12@inbox.ru>
2025-01-16 23:59:49 +0300

    Home_work 13.6.2

---
+++ Module13.Collections.HW-13.6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module13.Collections.HW_13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\12dol\\OneDrive\\Рабочий стол\\input.txt");
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var words = new Dictionary<string, int>();
+
+            foreach (var word in noPunctuationText.Split(" "))
+            {
+                if (words.ContainsKey(word))
+                {
+                    words[word]++;
+                }
+                else
+                {
+                    words.Add(word, 1);
+                }
+            }
+
+            var result = words.OrderByDescending(x => x.Value).First();
+
+            Console.WriteLine($"Максимально повтоярется: {result.Key} - Количество: {result.Value}");
+            Console.ReadLine();
+        }
+    }
+}
