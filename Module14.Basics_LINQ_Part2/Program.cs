51ef0283ad020d2874e350a4ee4866990dfad20e
andrey dolgih <12dolg12@inbox.ru>
2025-01-31 01:42:07 +0300

    Home_work 15.6.6

--- Module14.Basics_LINQ_Part2/Program.cs
+++ Module14.Basics_LINQ_Part2/Program.cs
@@ -8,54 +8,26 @@ namespace Module14.Basics_LINQ_Part2
     internal class Program
     {
         static void Main(string[] args)
-        {
-            var phoneBook = new List<Contact>();
-
-            // добавляем контакты
-            phoneBook.Add(new Contact("Игорь", 79990000001, "igor@example.com"));
-            phoneBook.Add(new Contact("Сергей", 79990000010, "serge@example.com"));
-            phoneBook.Add(new Contact("Анатолий", 79990000011, "anatoly@example.com"));
-            phoneBook.Add(new Contact("Валерий", 79990000012, "valera@example.com"));
-            phoneBook.Add(new Contact("Сергей", 799900000013, "serg@gmail.com"));
-            phoneBook.Add(new Contact("Иннокентий", 799900000013, "innokentii@example.com"));
-
-            var group = phoneBook.GroupBy(c => c.Mail.Split('@').Last());
-
-            foreach (var group1 in group)
+        {   
+            var classes = new[]
             {
-                if (group1.Key.Contains("example"))
-                {
-                    Console.WriteLine("Fake: ");
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
 
-                    foreach (var cont in group1)
-                    {
-                        Console.WriteLine(cont.Name + " " + cont.Mail);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Real: ");
-                    foreach(var cont in group1)
-                    {
-                        Console.WriteLine(cont.Name + " " + cont.Mail);
-                    }
-                }
-            }
+            Console.WriteLine(string.Join(" ", allStudents));
         }
-    }
-
-    class Contact
-    {
-        public Contact(string name, long phone, string mail)
+        // Используем метод SelectMany
+        static string[] GetAllStudents(Classroom[] classes)
         {
-            Name = name;
-            Phone = phone;
-            Mail = mail;
+            return classes.SelectMany(s  => s.Students).ToArray();
         }
+    }    
 
-        public string Name { get; set; }
-        public long Phone { get; set; }
-        public string Mail { get; set; }
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
     }
 }
