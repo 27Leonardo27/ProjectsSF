8a33b96c003a74f220448a2b298d4cc7fdc49185
andrey dolgih <12dolg12@inbox.ru>
2025-01-30 23:15:07 +0300

    task 15.2.8

---
+++ Module14.Basics_LINQ_Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+
+namespace Module14.Basics_LINQ_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listNumbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Введите число: ");
+
+                var input = Console.ReadLine();
+                var isInt = int.TryParse(input, out int isNums);
+
+                if (!isInt)
+                {
+                    Console.WriteLine("Вы ввели не число!");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    listNumbers.Add(isNums);
+                    Console.WriteLine($"Всего в списке - {listNumbers.Count}" +
+                         $"\nСумма всех чисел списка - {listNumbers.Sum()}" +
+                         $"\nНаименьшее число - {listNumbers.Min()}" +
+                         $"\nНаибольшее число - {listNumbers.Max()}" +
+                         $"\nСреднее значение всех чисел - {listNumbers.Average()}");
+                }
+            }
+        }
+    } 
+}
+    
