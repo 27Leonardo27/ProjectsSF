7e552ad22df4143c8b9ca76879337f5a151953f0
andrey dolgih <12dolg12@inbox.ru>
2025-02-13 00:54:51 +0300

    Home_work 17.6.6

---
+++ Module.17.Patterns_Part1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.AccessControl;
+
+namespace Module._17.Patterns_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    //создаем отдельный интерфейс
+    public interface IAccount
+    {
+        public double Balance { get; set; }
+        public double Interest { get; }
+
+        public void CalculateInterest();
+    }
+    //различные классы соответствующего типа
+    public class SalaryAccount : IAccount
+    {
+        public double Balance { get; set; }
+        public double Interest { get; private set; }
+
+        public void CalculateInterest()
+        {
+            Interest = Balance * 0.5;
+        }
+    }
+
+    public class NormalAccount : IAccount
+    {
+        public double Balance { get ; set; }
+        public double Interest { get; private set; }
+
+        public void CalculateInterest()
+        {
+            Interest = Balance * 0.4;
+
+            if (Balance < 1000)
+                Interest -= Balance * 0.2;
+
+            if (Balance >= 1000)
+                Interest -= Balance * 0.4;
+        }
+    }
+
+    public static class Calculator
+    {
+        public static void CalculateInterest(IAccount account)
+        {
+            account.CalculateInterest();
+        }
+    }
+}
